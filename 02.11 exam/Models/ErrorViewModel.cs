08ab6eadc00dc8a6dcf1545e38c3307290547c6e
evdokim <evdokim@gmail.com>
2020-02-12 20:10:17 +0200

    first commit

---
+++ 02.11 exam/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _02._11_exam.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
